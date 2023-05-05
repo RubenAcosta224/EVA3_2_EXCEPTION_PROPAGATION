90beb15b0f8f6739eea8c9f69083ff5881e49e65
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-05-04 22:14:20 -0600

    Add files via upload

---
+++ EVA3_2_EXCEPTION_PROPAGATION/src/eva3_2_exception_propagation/EVA3_2_EXCEPTION_PROPAGATION.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_2_exception_propagation;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_2_EXCEPTION_PROPAGATION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Inicia main");
+        
+        
+        A();
+        
+        
+        System.out.println("Termina main");
+        
+    }
+    
+    public static void A(){
+        
+                System.out.println("Inicia A");
+        
+        B();
+        System.out.println("Termina A");
+        
+    }
+    
+        public static void B(){
+            
+                    System.out.println("Inicia B");
+        
+        C();
+        System.out.println("Termina B");
+        
+    }
+        
+            public static void C(){
+                
+                        System.out.println("Inicia C");
+        int x=3,y=0;
+        int res=x/y;
+                System.out.println("Resultado: "+res);
+        
+        System.out.println("Termina C");
+        
+    }
+    
+}
